6bb0138d1d210f18886e55b4fed89a6c17884454
Alexey Aksenov <ezh@ezh.msk.ru>
2021-02-16 06:38:03 +0300

    Remove backend ETCDv2

--- backend/init/init.go
+++ backend/init/init.go
@@ -17,7 +17,6 @@ import (
 	backendAzure "github.com/hashicorp/terraform/backend/remote-state/azure"
 	backendConsul "github.com/hashicorp/terraform/backend/remote-state/consul"
 	backendCos "github.com/hashicorp/terraform/backend/remote-state/cos"
-	backendEtcdv2 "github.com/hashicorp/terraform/backend/remote-state/etcdv2"
 	backendEtcdv3 "github.com/hashicorp/terraform/backend/remote-state/etcdv3"
 	backendGCS "github.com/hashicorp/terraform/backend/remote-state/gcs"
 	backendHTTP "github.com/hashicorp/terraform/backend/remote-state/http"
@@ -59,7 +58,6 @@ func Init(services *disco.Disco) {
 		"azurerm":     func() backend.Backend { return backendAzure.New() },
 		"consul":      func() backend.Backend { return backendConsul.New() },
 		"cos":         func() backend.Backend { return backendCos.New() },
-		"etcd":        func() backend.Backend { return backendEtcdv2.New() },
 		"etcdv3":      func() backend.Backend { return backendEtcdv3.New() },
 		"gcs":         func() backend.Backend { return backendGCS.New() },
 		"http":        func() backend.Backend { return backendHTTP.New() },
